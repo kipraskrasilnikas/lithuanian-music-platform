b227d893d99816d0fe2bdc60073d576c1b31c1f3
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-03-03 13:59:47 +0200

    Add/remove location form fields in profile page dinamically (front-end only)

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -42,7 +42,118 @@
                 <label for="passwordConfirmationInput" class="form-label">Confirm password</label>
                 <input type="password" class="form-control" name="password_confirmation">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="mb-3" style="font-size: 30px;">
+                <label class="form-label">Locations</label>
+            </div>
+            <div id="locations">
+                <div id="location-template" style="display: none;">
+                    <div class="mb-3 location">
+                        <div class="mb-3" style="font-size: 20px;">
+                            <label class="form-label" id="location-name"></label>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">County</label>
+                            <select class="form-control" name="locations[][county]" required>
+                                <option value="">Select County</option>
+                                @foreach ($counties as $county)
+                                    <option value="{{ $county }}">{{ $county }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">City</label>
+                            <input type="text" class="form-control" name="locations[][city]" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Address</label>
+                            <input type="text" class="form-control" name="locations[][address]">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Postcode</label>
+                            <input type="text" class="form-control" name="locations[][postcode]">
+                        </div>
+                        <div class="mb-3">
+                            <button type="button" class="btn btn-danger delete-location">Delete</button>
+                        </div>
+                    </div>
+                </div>
+    
+                @foreach ($user->locations as $index => $location)
+                    <div data-id="{{ $index }}" class="mb-3 location">
+                        <div class="mb-3" style="font-size: 20px;">
+                            <label class="form-label" id="location-name">Location {{$index}}</label>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">County</label>
+                            <select class="form-control" name="locations[][county]" required>
+                                <option value="">Select County</option>
+                                @foreach ($counties as $county)
+                                    <option value="{{ $county }}" @if ($county === $location->county) selected @endif>{{ $county }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">City</label>
+                            <input type="text" class="form-control" name="locations[][city]" value="{{ $location->city }}" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Address</label>
+                            <input type="text" class="form-control" name="locations[][address]" value="{{ $location->address }}">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Postcode</label>
+                            <input type="text" class="form-control" name="locations[][postcode]" value="{{ $location->postcode }}">
+                        </div>
+                        <div class="mb-3">
+                            <button type="button" class="btn btn-danger delete-location">Delete</button>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="mb-3">
+                <button type="button" class="btn btn-primary" id="add-location">Add Location</button>
+            </div>
+            <div class="mb-3 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
         </form>
     </div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            document.getElementById("add-location").addEventListener("click", function() {
+                var locationsCount = document.querySelectorAll(".location").length;
+                if (locationsCount < 4) {
+                    // gaunam paskutinio indeksa, jei nera, priskiriam 0
+                    var index = $("#locations").find(".location:last").data("id") ? ($("#locations").find(".location:last").data("id") + 1): 1;
+
+                    var locationTemplateClone = $("#location-template").children().clone();
+                    $("#locations").append(locationTemplateClone);
+                    
+                    var lastLocation = $(".location:last");
+
+                    lastLocation.attr("data-id", index);
+                    lastLocation.find("#location-name").text("Location " + index);
+                } else {
+                    alert("You can only add up to 3 locations.");
+                }
+            });
+
+            // Event listener for delete buttons
+            $(document).on("click", ".delete-location", function() {
+                // Remove the parent location div
+                $(this).closest(".location").remove();
+
+                // Update the indices of the remaining locations
+                $(".location").each(function(index) {
+                    $(this).attr("data-id", index);
+                    $(this).find("#location-name").text("Location " + (index));
+                });
+            });
+
+        });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 @endsection
