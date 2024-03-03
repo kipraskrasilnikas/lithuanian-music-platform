1fa7a49ef5b7589a29bf1d599ee792fb77e69251
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-03-03 09:14:35 +0200

    Add locations table

---
+++ app/Models/Location.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'country',
+        'region',
+        'city',
+        'address',
+    ];
+
+    /**
+     * Get the user that owns the location.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
