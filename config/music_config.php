e6910c7d569ad0e3b8b24a7f81ae185229878daa
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-03-10 16:09:50 +0200

    Create config file to hold array values for genres, specialties and counties

---
+++ config/music_config.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'genres' => [
+        'Alternatyva',
+        'Džiazas ir Bliuzas',
+        'Elektroninė muzika',
+        'Folkas, dainavimas ir dainų rašymas',
+        'Garso takelis',
+        'Hip-hopas ir Repas',
+        'Indie',
+        'Kantri',
+        'Klasika',
+        'Latin',
+        'Metalas',
+        'Pasaulinis',
+        'Pianinas',
+        'Popas',
+        'R&B ir Siela',
+        'Regė',
+        'Reggaetonas',
+        'Rokas',
+        'Šokio muzika ir EDM',
+        'Techno',
+        'Trance',
+        'Trapas',
+        'Triphopas',
+    ],
+
+    'specialties' => [
+        'Vokalistas',
+        'Instrumentalistas',
+        'DJ',
+        'Prodiuseris',
+        'Garso inžinierius',
+        'Mixinimo inžinierius',
+        'Masterinimo inžinierius',
+        'Kompozitorius',
+        'Dainų autorius',
+        'Foninis vokalistas',
+        'Bytbokseris'
+    ],
+
+    'counties' => [
+        'Alytus',
+        'Kaunas',
+        'Klaipėda',
+        'Marijampolė',
+        'Panevėžys',
+        'Šiauliai',
+        'Tauragė',
+        'Telšiai',
+        'Utena',
+        'Vilnius'
+    ]
+];
