979fa9877f38dfeba5571620502a9b899336db3a
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-03-10 16:39:02 +0200

    Use music constants for genre, specialties and counties in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -17,60 +17,10 @@ class ProfileController extends Controller
 
             $locations = Location::Where('user_id', $user->id)->get();
 
-            // Define the list of Lithuanian counties
-            $counties = [
-                'Alytus',
-                'Kaunas',
-                'Klaipėda',
-                'Marijampolė',
-                'Panevėžys',
-                'Šiauliai',
-                'Tauragė',
-                'Telšiai',
-                'Utena',
-                'Vilnius'
-            ];
+            $counties = config('music_config.counties');
+            $genres = config('music_config.genres');
+            $specialties = config('music_config.specialties');
 
-            $genres = [
-                'Alternatyva',
-                'Džiazas ir Bliuzas',
-                'Elektroninė muzika',
-                'Folkas, dainavimas ir dainų rašymas',
-                'Garso takelis',
-                'Hip-hopas ir Repas',
-                'Indie',
-                'Kantri',
-                'Klasika',
-                'Latin',
-                'Metalas',
-                'Pasaulinis',
-                'Pianinas',
-                'Popas',
-                'R&B ir Siela',
-                'Regė',
-                'Reggaetonas',
-                'Rokas',
-                'Šokio muzika ir EDM',
-                'Techno',
-                'Trance',
-                'Trapas',
-                'Triphopas',
-            ];
-                        
-            $specialties = [
-                'Vokalistas',
-                'Instrumentalistas', // Bendra parinktis muzikantėms, kurios pagrindiniu būdu groja instrumentais
-                'DJ',
-                'Prodiuseris',
-                'Garso inžinierius',
-                'Mixinimo inžinierius',
-                'Masterinimo inžinierius',
-                'Kompozitorius',
-                'Dainų autorius',
-                'Reperis',
-                'Foninis vokalistas',
-                'Bytbokseris'
-            ];
                         
             return view('profile', compact('user', 'counties', 'genres', 'specialties', 'locations'));
         }
