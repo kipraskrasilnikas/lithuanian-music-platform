ade720a900f0e55ebbdd9f9461e3ae58e779306f
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-03-20 13:42:07 +0200

    Fix header to match homepage

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -20,37 +20,24 @@
       <!-- Nav Menu -->
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="index.html#hero">Home</a></li>
-          <li><a href="index.html#about">About</a></li>
-          <li><a href="index.html#services">Services</a></li>
-          <li><a href="index.html#portfolio">Portfolio</a></li>
-          <li><a href="index.html#team">Team</a></li>
-          <li><a href="blog.html" class="active">Blog</a></li>
-          <li class="dropdown has-dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down"></i></a>
-            <ul class="dd-box-shadow">
-              <li><a href="#">Dropdown 1</a></li>
-              <li class="dropdown has-dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down"></i></a>
-                <ul class="dd-box-shadow">
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Dropdown 2</a></li>
-              <li><a href="#">Dropdown 3</a></li>
-              <li><a href="#">Dropdown 4</a></li>
-            </ul>
-          </li>
-          <li><a href="index.html#contact">Contact</a></li>
-        </ul>
+            <li><a href="{{ route('home') }}#hero" class="active">Namų puslapis</a></li>
+            @auth
+              <li><a href="{{ route('search') }}">Muzikantų paieška</a></li>
+              <li><a href="{{ route('profile') }}">Mano profilis</a></li>
+            @endauth
+          </ul>
 
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav><!-- End Nav Menu -->
 
-      <a class="btn-getstarted" href="index.html#about">Get Started</a>
-
+      @auth
+        <a class="btn-getstarted" href="{{ route('logout') }}">Atsijungti</a>
+      @else
+      <div class="btn-getstarted-group">
+        <a class="btn-getstarted" href="{{ route('registration') }}">Registruotis</a>
+        <a class="btn-getstarted" href="{{ route('login') }}">Prisijungti</a>
+      </div>
+      @endauth
     </div>
   </header><!-- End Header -->
                   
