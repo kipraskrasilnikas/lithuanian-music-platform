604971313f7775056eee7c77f63658c329c6dea8
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-03-20 18:37:13 +0200

    Search form remove old element and add message when no users are found

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -58,25 +58,36 @@
                     <div class="col-lg-8 pr-lg-4">
                         <div class="row">
                             <div class="col-md-12">
-                                @foreach ($users as $user)
-                                    <div class="team d-md-flex p-4 bg-white">
-                                        <div class="img" style="background-image: url(images/person_2.jpg);"></div>
-                                        <div class="text pl-md-4">
-                                            @php
-                                                $counties = $user->locations->pluck('county')->implode(', ');
-                                                $genres = $user->genres->pluck('name')->implode(', ');
-                                                $specialties = $user->specialties->pluck('name')->implode(', ');
-                                            @endphp
-                                            <span class="location mb-0">{{ $counties }}</span>
-                                            <h2>{{ $user->name }}</h2>
-                                            <span class="position">{{ $genres }}</span>
-                                            <p class="mb-2">{{ $specialties }}</p>
-                                            <p><a href="#" class="btn btn-primary">Susisiekti</a></p>
+                                @if ($users->isEmpty())
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="alert alert-danger" role="alert">
+                                                    Pagal nurodytus paieškos kriterijus, muzikantų nerasta.
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                @endforeach
+                                @else
+                                    @foreach ($users as $user)
+                                        <div class="team d-md-flex p-4 bg-white">
+                                            <div class="img" style="background-image: url(images/person_2.jpg);"></div>
+                                            <div class="text pl-md-4">
+                                                @php
+                                                    $counties = $user->locations->pluck('county')->implode(', ');
+                                                    $genres = $user->genres->pluck('name')->implode(', ');
+                                                    $specialties = $user->specialties->pluck('name')->implode(', ');
+                                                @endphp
+                                                <span class="location mb-0">{{ $counties }}</span>
+                                                <h2>{{ $user->name }}</h2>
+                                                <span class="position">{{ $genres }}</span>
+                                                <p class="mb-2">{{ $specialties }}</p>
+                                                <p><a href="#" class="btn btn-primary">Susisiekti</a></p>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                @endif
                             </div>
-                               
                         </div>
                         <div class="row mt-5">
                             <div class="col text-center">
@@ -141,14 +152,6 @@
                 </div>
             </div>
         </section>
-
-        <!-- loader -->
-        <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4"
-                stroke="#eeeeee" />
-            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4"
-                stroke-miterlimit="10" stroke="#F96D00" />
-        </svg></div>
     </body>
     </html>
 @endsection
