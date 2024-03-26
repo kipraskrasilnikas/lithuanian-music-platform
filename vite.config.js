9c01a6886c201900617b5cead4eb35c7e48d7997
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-03-26 19:03:09 +0200

    Add and install chatify

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        }
+    }
 });
