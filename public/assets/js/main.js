dc60614d21a716f7d563d090c85c9384c15f9c70
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-04-03 20:33:08 +0300

    Fix js errors

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -39,83 +39,85 @@
           behavior: 'smooth'
         });
       });
-    }
 
-    window.addEventListener('load', toggleScrollTop);
-    document.addEventListener('scroll', toggleScrollTop);
+      window.addEventListener('load', toggleScrollTop);
+      document.addEventListener('scroll', toggleScrollTop);
 
-    /**
-     * Apply .scrolled class to the body as the page is scrolled down
-     */
-    function toggleScrolled() {
-      const selectBody = document.querySelector('body');
-      const selectHeader = document.querySelector('#header');
-      if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-      window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-    }
-
-    document.addEventListener('scroll', toggleScrolled);
-    window.addEventListener('load', toggleScrolled);
-
-    /**
-     * Scroll up sticky header to headers with .scroll-up-sticky class
-     */
-    let lastScrollTop = 0;
-    window.addEventListener('scroll', function() {
-      const selectHeader = document.querySelector('#header');
-      if (!selectHeader.classList.contains('scroll-up-sticky')) return;
-
-      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-      if (scrollTop > lastScrollTop && scrollTop > selectHeader.offsetHeight) {
-        selectHeader.style.setProperty('position', 'sticky', 'important');
-        selectHeader.style.top = `-${header.offsetHeight + 50}px`;
-      } else if (scrollTop > selectHeader.offsetHeight) {
-        selectHeader.style.setProperty('position', 'sticky', 'important');
-        selectHeader.style.top = "0";
-      } else {
-        selectHeader.style.removeProperty('top');
-        selectHeader.style.removeProperty('position');
+      /**
+       * Apply .scrolled class to the body as the page is scrolled down
+       */
+      function toggleScrolled() {
+        const selectBody = document.querySelector('body');
+        const selectHeader = document.querySelector('#header');
+        if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+        window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
       }
-      lastScrollTop = scrollTop;
-    });
+
+      document.addEventListener('scroll', toggleScrolled);
+      window.addEventListener('load', toggleScrolled);
+
+      /**
+       * Scroll up sticky header to headers with .scroll-up-sticky class
+       */
+      let lastScrollTop = 0;
+      window.addEventListener('scroll', function() {
+        const selectHeader = document.querySelector('#header');
+        if (!selectHeader.classList.contains('scroll-up-sticky')) return;
+
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop && scrollTop > selectHeader.offsetHeight) {
+          selectHeader.style.setProperty('position', 'sticky', 'important');
+          selectHeader.style.top = `-${header.offsetHeight + 50}px`;
+        } else if (scrollTop > selectHeader.offsetHeight) {
+          selectHeader.style.setProperty('position', 'sticky', 'important');
+          selectHeader.style.top = "0";
+        } else {
+          selectHeader.style.removeProperty('top');
+          selectHeader.style.removeProperty('position');
+        }
+        lastScrollTop = scrollTop;
+      });
+    }
 
     /**
      * Mobile nav toggle
      */
     const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-    function mobileNavToogle() {
-      document.querySelector('body').classList.toggle('mobile-nav-active');
-      mobileNavToggleBtn.classList.toggle('bi-list');
-      mobileNavToggleBtn.classList.toggle('bi-x');
+    if (mobileNavToggleBtn) {
+      function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavToggleBtn.classList.toggle('bi-list');
+        mobileNavToggleBtn.classList.toggle('bi-x');
+      }
+      mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+      /**
+       * Hide mobile nav on same-page/hash links
+       */
+      document.querySelectorAll('#navmenu a').forEach(navmenu => {
+        navmenu.addEventListener('click', () => {
+          if (document.querySelector('.mobile-nav-active')) {
+            mobileNavToogle();
+          }
+        });
+      });
+
+      /**
+       * Toggle mobile nav dropdowns
+       */
+      document.querySelectorAll('.navmenu .has-dropdown i').forEach(navmenu => {
+        navmenu.addEventListener('click', function(e) {
+          if (document.querySelector('.mobile-nav-active')) {
+            e.preventDefault();
+            this.parentNode.classList.toggle('active');
+            this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+            e.stopImmediatePropagation();
+          }
+        });
+      });
     }
-    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-
-    /**
-     * Hide mobile nav on same-page/hash links
-     */
-    document.querySelectorAll('#navmenu a').forEach(navmenu => {
-      navmenu.addEventListener('click', () => {
-        if (document.querySelector('.mobile-nav-active')) {
-          mobileNavToogle();
-        }
-      });
-    });
-
-    /**
-     * Toggle mobile nav dropdowns
-     */
-    document.querySelectorAll('.navmenu .has-dropdown i').forEach(navmenu => {
-      navmenu.addEventListener('click', function(e) {
-        if (document.querySelector('.mobile-nav-active')) {
-          e.preventDefault();
-          this.parentNode.classList.toggle('active');
-          this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-          e.stopImmediatePropagation();
-        }
-      });
-    });
 
     /**
      * Correct scrolling position upon page load for URLs containing hash links.
