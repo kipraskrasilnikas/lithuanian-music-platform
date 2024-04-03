cbfdca9cf206053c86badc95ebccb32e841001b6
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-04-03 20:22:36 +0300

    Working create form with front-end field validation and image preview

--- resources/views/resources/create.blade.php
+++ resources/views/resources/create.blade.php
@@ -3,39 +3,89 @@
 <div class="card" style="margin:20px;">
   <div class="card-header">Įkelti naują resursą</div>
   <div class="card-body">
-
-    <form action="{{ route('resources.store') }}" method="post">
+    <form action="{{ route('resources.store') }}" method="post" enctype="multipart/form-data">
         @csrf
 
         <label>Pavadinimas</label><br>
-        <input type="text" name="name" id="name" class="form-control"><br>
-
+        <input type="text" name="name" id="name" class="form-control" required>
+        @error('name')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    
         <label>Resurso tipas</label><br>
-        <select name="type" id="type" class="form-control">
+        <select name="type" id="type" class="form-control" required>
             @foreach(config('music_config.resource_types') as $type)
-            <option value="{{ $type }}">{{ $type }}</option>
+                <option value="{{ $type }}">{{ $type }}</option>
             @endforeach
-        </select><br>
-
+        </select>
+        @error('type')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    
         <label>Aprašymas</label><br>
-        <textarea name="description" id="description" class="form-control"></textarea><br>
-
-        <label>Paveikslėlis</label><br>
-        <input type="file" name="image" id="image" class="form-control"><br>
-
+        <textarea name="description" id="description" class="form-control"></textarea>
+        @error('description')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    
+        <label>Paveikslėlis</label>
+        <input type="file" name="image" id="image" class="form-control">
+        <img id="preview_image" src="#" alt="Preview" style="display: none; width: 100px; height: 100px;"><br>
+        @error('image')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    
         <label>Adresas</label><br>
-        <input type="text" name="address" id="address" class="form-control"><br>
-
+        <input type="text" name="address" id="address" class="form-control">
+        @error('address')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    
         <label>Telefono numeris</label><br>
-        <input type="tel" name="telephone" id="telephone" class="form-control"><br>
-
+        <input type="tel" name="telephone" id="telephone" class="form-control" pattern="[0-9]{9,}" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);">
+        @error('telephone')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    
         <label>Elektroninis paštas</label><br>
-        <input type="email" name="email" id="email" class="form-control"><br>
+        <input type="email" name="email" id="email" class="form-control">
+        @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
 
-        <input type="submit" value="Save" class="btn btn-success"><br>
+        <input type="submit" value="Įkelti" class="btn btn-success">
     </form>
-
   </div>
 </div>
+
+<script>
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#preview_image').attr('src', e.target.result);
+                $('#preview_image').show();
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#image").change(function(){
+        readURL(this);
+    });
+
+    function InvalidMsg(textbox) {
+        if (textbox.validity.patternMismatch){
+            textbox.setCustomValidity('Telefono numerį turi sudaryti bent 9 skaičiai!');
+        }    
+        else {
+            textbox.setCustomValidity('');
+        }
+
+        return true;
+    }
+</script>
 
 @stop
