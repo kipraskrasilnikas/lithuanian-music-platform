2f624c4f54f14f0de34c7f06daf536bd26cee273
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-04-10 17:36:06 +0300

    Resource create form add bottom padding to every form group

--- resources/views/resources/create.blade.php
+++ resources/views/resources/create.blade.php
@@ -7,27 +7,33 @@
     <form action="{{ route('resources.store') }}" method="post" enctype="multipart/form-data">
         @csrf
 
-        <label>Pavadinimas</label><br>
-        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required>
-        @error('name')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
+        <div class="mb-3">
+            <label>Pavadinimas</label><br>
+            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required>
+            @error('name')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
     
-        <label>Resurso tipas</label><br>
-        <select name="type" id="type" class="form-control" required>
-            @foreach(config('music_config.resource_types') as $type)
-                <option value="{{ $type }}" {{ old('type') == $type ? 'selected' : '' }}>{{ $type }}</option>
-            @endforeach
-        </select>
-        @error('type')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    
-        <label>Aprašymas</label><br>
-        <textarea name="description" id="description" class="form-control">{{ old('description') }}</textarea>
-        @error('description')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
+        <div class="mb-3">
+            <label>Resurso tipas</label><br>
+            <select name="type" id="type" class="form-control" required>
+                @foreach(config('music_config.resource_types') as $type)
+                    <option value="{{ $type }}" {{ old('type') == $type ? 'selected' : '' }}>{{ $type }}</option>
+                @endforeach
+            </select>
+            @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label>Aprašymas</label><br>
+            <textarea name="description" id="description" class="form-control">{{ old('description') }}</textarea>
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
     
         <label>Paveikslėlis</label>
         <input type="file" name="image" id="image" class="form-control">
@@ -36,23 +42,29 @@
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
     
-        <label>Adresas</label><br>
-        <input type="text" name="address" id="address" class="form-control" value="{{ old('address') }}">
-        @error('address')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    
-        <label>Telefono numeris</label><br>
-        <input type="tel" name="telephone" id="telephone" class="form-control" pattern="^\+?[0-9]{9,}$" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);" value="{{ old('telephone') }}">
-        @error('telephone')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    
-        <label>Elektroninis paštas</label><br>
-        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
-        @error('email')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
+        <div class="mb-3">
+            <label>Adresas</label><br>
+            <input type="text" name="address" id="address" class="form-control" value="{{ old('address') }}">
+            @error('address')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        
+        <div class="mb-3">
+            <label>Telefono numeris</label><br>
+            <input type="tel" name="telephone" id="telephone" class="form-control" pattern="^\+?[0-9]{9,}$" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);" value="{{ old('telephone') }}">
+            @error('telephone')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label>Elektroninis paštas</label><br>
+            <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
+            @error('email')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
         <a href="{{ route('resource') }}" class="btn btn-primary">Grįžti</a>
         <input type="submit" value="Įkelti" class="btn btn-success">
