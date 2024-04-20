4815eb57a424ee4d6bd3ad289d50ea82fa72e614
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-04-20 16:37:43 +0300

    Fix profile form styling after adding songs table

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -22,147 +22,154 @@
             @endif
         </div>
 
-        <form action="{{ route('profile.post') }}" method="POST" class="ms-auto me-auto mt-3" style="width: 700px">
+        <form action="{{ route('profile.post') }}" method="POST" class="ms-auto me-auto mt-3 form-center" style="width: 80%;">
             @csrf
-            <div class="mb-3" style="font-size: 30px;">
-                <label class="form-label">Bendra informacija</label>
+            <div style="text-align: center;">
+                <div class="mb-3" style="font-size: 30px;">
+                    <label class="form-label">Bendra informacija</label>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Vardas, Pavardė<span style="color: red;">*</span></label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}" style="width: 30%; margin-right" align="center">
+                    @error('name')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="loginEmailInput">El. pašto adresas<span style="color: red;">*</span></label>
+                    <input type="email" class="form-control" id="loginEmailInput" name="email" value="{{ $user->email }}">
+                    @error('email')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="passwordInput" class="form-label">Slaptažodis</label>
+                    <input type="password" class="form-control" name="password">
+                    @error('password')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="passwordConfirmationInput" class="form-label">Patvirtinti slaptažodį</label>
+                    <input type="password" class="form-control" name="password_confirmation">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Aprašymas</label>
+                    <textarea class="form-control" id="description" name="description" rows="3">{{ $user->description }}</textarea>
+                    <div id="description-counter" class="form-text text-muted">Characters left: <span id="description-count">{{ 500 - strlen($user->description) }}</span></div>
+                </div>
+                <div class="mb-3" style="font-size: 30px;">
+                    <label class="form-label">Muzikos specifikacija</label>
+                </div>
+                <div class="mb-3" style="font-size: 15px; margin-top: -30px;">
+                    <label class="form-label">(Nurodykite, kad jus rastų galimi kolaborantai)</label>
+                </div>
             </div>
-            <div class="mb-3">
-                <label class="form-label">Vardas, Pavardė<span style="color: red;">*</span></label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}">
-                @error('name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="loginEmailInput">El. pašto adresas<span style="color: red;">*</span></label>
-                <input type="email" class="form-control" id="loginEmailInput" name="email" value="{{ $user->email }}">
-                @error('email')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="passwordInput" class="form-label">Slaptažodis</label>
-                <input type="password" class="form-control" name="password">
-                @error('password')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="passwordConfirmationInput" class="form-label">Patvirtinti slaptažodį</label>
-                <input type="password" class="form-control" name="password_confirmation">
-            </div>
-            <div class="mb-3">
-                <label for="description" class="form-label">Aprašymas</label>
-                <textarea class="form-control" id="description" name="description" rows="3">{{ $user->description }}</textarea>
-                <div id="description-counter" class="form-text text-muted">Characters left: <span id="description-count">{{ 500 - strlen($user->description) }}</span></div>
-            </div>
-            <div class="mb-3" style="font-size: 30px;">
-                <label class="form-label">Muzikos specifikacija</label>
-            </div>
-            <div class="mb-3" style="font-size: 15px; margin-top: -30px;">
-                <label class="form-label">(Nurodykite, kad jus rastų galimi kolaborantai)</label>
-            </div>
-            <div class="mb-3">
-                <label for="specialtyInput" class="form-label">Specializacija<span style="color: red;">*</span></label>
-                <br>
-                @foreach (config('music_config.specialties') as $specialty)
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="specialty-{{ $loop->iteration }}" name="specialties[]" value="{{ $specialty }}" 
-                            {{ in_array($specialty, $user_specialties->pluck('name')->toArray()) ? 'checked' : '' }}>
-                        <label class="form-check-label" for="specialty-{{ $loop->iteration }}">
-                            {{ $specialty }}
-                        </label>
-                    </div>
-                @endforeach
-                @error('specialties')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>             
-            <div class="mb-3">
-                <label for="genreInput" class="form-label">Žanras<span style="color: red;">*</span></label>
-                <br>
-                @foreach (config('music_config.genres') as $genre)
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="genre-{{ $loop->iteration }}" name="genres[]" value="{{ $genre }}" 
-                            {{ in_array($genre, $user_genres->pluck('name')->toArray()) ? 'checked' : '' }}>
-                        <label class="form-check-label" for="specialty-{{ $loop->iteration }}">
-                            {{ $genre }}
-                        </label>
-                    </div>
-                @endforeach
-                @error('genres')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="moodInput" class="form-label">Jūsų muzikos nuotaikos</label>
-                <br>
-                @foreach (config('music_config.music_moods') as $mood_category => $mood_details)
-                    <li style="list-style-type: none;">
-                        <div class="form-check" style="color: {{ $mood_details['color_hex'] }}">
-                            <input class="form-check-input mood-category" type="checkbox" id="mood-category-{{ $loop->iteration}}">{{ $mood_category }}</label>
+           
+            <div style="margin: auto; width: 30%">
+                <div class="mb-3">
+                    <label for="specialtyInput" class="form-label">Specializacija<span style="color: red;">*</span></label>
+                    <br>
+                    @foreach (config('music_config.specialties') as $specialty)
+                        <div class="form-check checkboxes">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="specialties[]" value="{{ $specialty }}" {{ in_array($specialty, $user_specialties->pluck('name')->toArray()) ? 'checked' : '' }}>
+                                <span>{{ $specialty }}</span>
+                            </label>
                         </div>
-                        <ul style="list-style-type: none;">
-                            @foreach ($mood_details['moods'] as $mood)
-                            <li style="list-style-type: none;">
-                                <div class="form-check" style="color: {{ $mood_details['color_hex'] }}">
-                                    <input class="form-check-input subOption" type="checkbox" id="mood-{{ $loop->iteration }}" name="moods[]" value="{{ $mood }}" 
-                                        {{ in_array($mood, $user_moods->pluck('mood')->toArray()) ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="mood-{{ $loop->iteration }}">
-                                        {{ $mood }}
-                                    </label>
-                                </div>
-                            </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                @endforeach
-                @error('moods')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>                   
-            <div class="mb-3" style="font-size: 30px;">
+                    @endforeach
+                    @error('specialties')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>             
+                <div class="mb-3">
+                    <label for="genreInput" class="form-label">Žanras<span style="color: red;">*</span></label>
+                    <br>
+                    @foreach (config('music_config.genres') as $genre)
+                        <div class="form-check checkboxes">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="genres[]" value="{{ $genre }}" {{ in_array($genre, $user_genres->pluck('name')->toArray()) ? 'checked' : '' }}>
+                                <span>{{ $genre }}</span>
+                            </label>
+                        </div>
+                    @endforeach
+                    @error('genres')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="moodInput" class="form-label">Jūsų muzikos nuotaikos</label>
+                    <br>
+                    @foreach (config('music_config.music_moods') as $mood_category => $mood_details)
+                        <li style="list-style-type: none;">
+                            <div class="form-check" style="color: {{ $mood_details['color_hex'] }}; display: flex; align-items: center;">
+                                <label class="form check-label">
+                                    <input class="form-check-input mood-category" type="checkbox" id="mood-category-{{ $loop->iteration}}">
+                                    <span>{{ $mood_category }}</span>
+                                </label>
+                            </div>
+                            <ul style="list-style-type: none;">
+                                @foreach ($mood_details['moods'] as $mood)
+                                <li style="list-style-type: none;">
+                                    <div class="form-check checkboxes" style="color: {{ $mood_details['color_hex'] }}">
+                                        <label class="form-check-label" for="mood-{{ $loop->iteration }}">
+                                            <input class="form-check-input subOption" type="checkbox" id="mood-{{ $loop->iteration }}" name="moods[]" value="{{ $mood }}" {{ in_array($mood, $user_moods->pluck('mood')->toArray()) ? 'checked' : '' }}>
+                                            <span>{{ $mood }}</span>
+                                        </label>
+                                    </div>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                    @error('moods')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+            <div class="mb-3" style="font-size: 30px; text-align: center;">
                 <label class="form-label">Vietos (galima iki 3-jų)</label>
             </div>
-            <table class="table table-bordered" id="location_table">
-                <tr>
-                    <th>Apskritis<span style="color: red;">*</span></th>
-                    <th>Miestas<span style="color: red;">*</span></th>
-                    <th>Adresas</th>
-                    <th>Veiksmas</th>
-                </tr>
-                <tr>
-                    <td>
-                        <select name="locations[0][county]" class="form-control">
-                            <option value="">Pasirinkti apskritį </option>
-                            @foreach (config('music_config.counties') as $county)
-                                <option value="{{ $county }}" {{ $locations[0]->county == $county ? 'selected' : '' }} >{{ $county }}</option>
-                            @endforeach
-                        </select>                    
-                    </td>
-                    <td>
-                        <input type="text" name="locations[0][city]" value="{{ $locations[0]->city }}" placeholder="Įveskite miestą" class="form-control">
-                    </td>
-                    <td>
-                        <input type="text" name="locations[0][address]" value="{{ $locations[0]->address }}" placeholder="Įveskite adresą" class="form-control">
-                    </td>
-                    <td>
-                        <button type="button" name="add" id="add_location" class="btn btn-success">Pridėti daugiau</button>
-                    </td>
-                </tr>
-            </table>
+            <div class="mb-3">
+                <table class="table table-bordered" id="location_table" style="margin: auto; width: 60%">
+                    <tr>
+                        <th>Apskritis<span style="color: red;">*</span></th>
+                        <th>Miestas<span style="color: red;">*</span></th>
+                        <th>Adresas</th>
+                        <th>Veiksmas</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select name="locations[0][county]" class="form-control">
+                                <option value="">Pasirinkti apskritį </option>
+                                @foreach (config('music_config.counties') as $county)
+                                    <option value="{{ $county }}" {{ $locations[0]->county == $county ? 'selected' : '' }} >{{ $county }}</option>
+                                @endforeach
+                            </select>                    
+                        </td>
+                        <td>
+                            <input type="text" name="locations[0][city]" value="{{ $locations[0]->city }}" placeholder="Įveskite miestą" class="form-control">
+                        </td>
+                        <td>
+                            <input type="text" name="locations[0][address]" value="{{ $locations[0]->address }}" placeholder="Įveskite adresą" class="form-control">
+                        </td>
+                        <td>
+                            <button type="button" name="add" id="add_location" class="btn btn-success">Pridėti daugiau</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
             @error('locations.*.county')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
             @error('locations.*.city')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-            <div class="mb-3" style="font-size: 30px;">
+            <div class="mb-3" style="font-size: 30px; text-align: center;">
                 <label class="form-label">Jūsų dainos (galima iki 7-ių)</label>
             </div>
-            <div class="d-flex justify-content-center" style="width: 150%;">
-                <table class="table table-bordered" id="song_table">
+            <div class="d-flex justify-content-center">
+                <table class="table table-bordered" id="songs_table">
                     <tr>
                         <th>Pavadinimas<span style="color: red;">*</span></th>
                         <th>Nuoroda<span style="color: red;">*</span></th>
@@ -201,12 +208,19 @@
                     </tr>
                 </table>
             </div>
-            <div class="mb-3" style="font-size: 30px;">
+            <div class="mb-3" style="font-size: 30px; text-align: center;">
                 <label class="form-label">Naudotojo statusas</label>
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" name="status" value="1" {{ $user->status == 1 ? 'checked' : '' }}>
-                <label class="form-check-label" for="status">Aktyvus</label>
+
+            <div style="margin: auto; width: 10%">
+                <div class="mb-5">
+                    <div class="form-check checkboxes">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" name="status" value="1" {{ $user->status == 1 ? 'checked' : '' }}>
+                            <span>Aktyvus</span>
+                        </label>
+                    </div>
+                </div>
             </div>
             <div class="mb-3 text-center">
                 <button type="submit" class="btn btn-primary">Išsaugoti</button>
