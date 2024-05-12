89143606e94aad8d79bb49f384a005b1e159a17a
kipraskrasilnikas <kipras.krasilnikas@gmail.com>
2024-05-12 07:33:43 +0300

    Fix navigation menu not highlighting when in music, songs and artists pages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -58,7 +58,7 @@
             <nav id="navmenu" class="navmenu">
                 <ul>
                     <li><a href="{{ route('home') }}#hero" class="{{ request()->is('/') ? 'active' : '' }}">Namų puslapis</a></li>
-                    <li><a href="{{ route('music') }}" class="{{ request()->is('/music') ? 'active' : '' }}">Atraskite muziką</a></li>
+                    <li><a href="{{ route('music') }}" class="{{ request()->is('music') || request()->is('searchSongs') || request()->is('searchArtists') ? 'active' : '' }}">Atraskite muziką</a></li>
                     <li><a href="{{ route('search') }}" class="{{ request()->is('search') ? 'active' : '' }}">Muzikantų paieška</a></li>
                     <li><a href="{{ route('resources') }}" class="{{ request()->is('resources') || request()->is('resource*') ? 'active' : '' }}">Ištekliai</a></li>
 
